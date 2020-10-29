74abc3531872e09f1522ef8e8fe7d13954a980a2
Danielle Lucaj <danielle.lucaj@gmail.com>
2020-10-29 10:53:54 -0400

    added ADD button, working on receipts

--- script.js
+++ script.js
@@ -68,18 +68,46 @@ let display = () => {
   inventory.forEach((item, index) => {
     let card = document.createElement("div");
     card.classList.add("item");
+
     let type = document.createElement("p");
     type.classList.add(item.type);
+    let itemInfo = document.createElement("div");
+    itemInfo.classList.add("item-info");
+    // itemInfo.innerText = (item.name, item.price);
     let name = document.createElement("p");
     name.innerText = item.name;
     let price = document.createElement("p");
     price.innerText = item.price;
-    card.append(type, name, price);
+    itemInfo.append(name, price);
+    let addToCart = document.createElement("button");
+    addToCart.classList.add("add");
+    addToCart.innerText = "Add";
+    addToCart.setAttribute("price", index);
+    card.append(type, name, price, addToCart);
     inventoryContainer.append(card);
-    if (item.quantity === 0) {
-      let out = document.createElement("div");
-      out.classList.add("hidden");
-    }
   });
 };
 display();
+
+let receiptContainer = document.querySelector(".receipt-container");
+let addToCart = document.querySelector(".add");
+let item = document.querySelector(".item");
+
+addToCart.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.classList.contains("add")) {
+    let name = document.createElement("p");
+    inventory.name.innerText = name;
+    let price = document.createElement("p");
+    price.innerText = price;
+    let index = e.target.getAttribute("data-price");
+    price.setAttribute("data-price", index);
+
+    let deleteFromCart = document.createElement("button");
+    deleteFromCart.classList.add("delete");
+    deleteFromCart.innerText = "Remove From Cart";
+    deleteFromCart.setAttribute(index, "price");
+    receiptContainer.append(name, inventory.price, deleteFromCart);
+    console.log(addToCart);
+  }
+});
